05d496110d735e3e3d969430695012ea62302a2f
ankithapai <ankithapai@ymail.com>
2022-12-04 23:14:19 +0530

    the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private http: HttpClient){}
+
   title = 'quote-generator';
+  value: any;
+
+  ngOnInit(){
+    this.generateQuote();
+  }
+
+
+  generateQuote() {
+    let headers = new HttpHeaders({
+      'X-RapidAPI-Key': '66c923cca7msh562749a28c7174ap1bfb7cjsn4d7dbd891334',
+      'X-RapidAPI-Host': 'free-famous-quotes.p.rapidapi.com'
+    });
+    this.http
+      .get<any>('https://free-famous-quotes.p.rapidapi.com/', {
+        headers: headers
+      })
+      .subscribe(data => {
+        this.value = data;
+
+      });
+  }
 }
